ea26cca5b2a330485309512e16861c0fd010f6d4
akash1520 <akashparmar1520@gmail.com>
2024-01-17 01:33:16 +0530

    chore : re-initialization

---
+++ src/main.js
@@ -0,0 +1,225 @@
+import "./style.css";
+
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  onSnapshot,
+  addDoc,
+  updateDoc,
+  getDoc,
+  deleteDoc
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASx-LMcLeOWHasC4tN6Pa7o5tixgd2J-4",
+  authDomain: "learnwebrtc-518a7.firebaseapp.com",
+  projectId: "learnwebrtc-518a7",
+  storageBucket: "learnwebrtc-518a7.appspot.com",
+  messagingSenderId: "136987443781",
+  appId: "1:136987443781:web:eff3690148a33fa9ca192c",
+};
+
+let app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+// Global State
+const pc = new RTCPeerConnection(servers);
+let localStream = null;
+let remoteStream = null;
+
+let hangCallDoc = null;
+
+
+// HTML elements
+const webcamButton = document.getElementById("webcamButton");
+const webcamVideo = document.getElementById("webcamVideo");
+const callButton = document.getElementById("callButton");
+const callInput = document.getElementById("callInput");
+const answerButton = document.getElementById("answerButton");
+const remoteVideo = document.getElementById("remoteVideo");
+const hangupButton = document.getElementById("hangupButton");
+
+// 1. Setup media sources
+
+webcamButton.onclick = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: { width: { min: 1280 }, height: { min: 720 } },
+    audio: { noiseSuppression: true, echoCancellation: true },
+  });
+  remoteStream = new MediaStream();
+
+  // Push tracks from local stream to peer connection
+  localStream.getTracks().forEach((track) => {
+    pc.addTrack(track, localStream);
+  });
+
+  // Pull tracks from remote stream, add to video stream
+  pc.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  webcamVideo.srcObject = localStream;
+  remoteVideo.srcObject = remoteStream;
+
+  callButton.disabled = false;
+  answerButton.disabled = false;
+  webcamButton.disabled = true;
+};
+
+// 2. Create an offer
+callButton.onclick = async () => {
+  // Reference Firestore collections for signaling
+  const callDoc = doc(collection(firestore, "calls"));
+  const offerCandidates = collection(callDoc, "offerCandidates");
+  const answerCandidates = collection(callDoc, "answerCandidates");
+
+  callInput.value = callDoc.id;
+
+  // Get candidates for caller, save to db
+  pc.onicecandidate = (event) => {
+    event.candidate && addDoc(offerCandidates, event.candidate.toJSON());
+  };
+
+  // Create offer
+  const offerDescription = await pc.createOffer();
+  await pc.setLocalDescription(offerDescription);
+
+  const offer = {
+    sdp: offerDescription.sdp,
+    type: offerDescription.type,
+  };
+
+  await setDoc(callDoc, { offer, callActive: true }); 
+
+  // Listen for remote answer
+  onSnapshot(callDoc, (snapshot) => {
+    const data = snapshot.data();
+    if (!pc.currentRemoteDescription && data?.answer) {
+      const answerDescription = new RTCSessionDescription(data.answer);
+      pc.setRemoteDescription(answerDescription);
+    }
+  });
+
+  // When answered, add candidate to peer connection
+  onSnapshot(answerCandidates, (snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === "added") {
+        const candidate = new RTCIceCandidate(change.doc.data());
+        pc.addIceCandidate(candidate);
+      }
+    });
+  });
+
+  hangupButton.disabled = false;
+};
+
+// 3. Answer the call with the unique ID
+answerButton.onclick = async () => {
+  const callId = callInput.value;
+  const callDoc = doc(firestore, "calls", callId);
+  const answerCandidates = collection(callDoc, "answerCandidates");
+  const offerCandidates = collection(callDoc, "offerCandidates");
+
+  pc.onicecandidate = (event) => {
+    event.candidate && addDoc(answerCandidates, event.candidate.toJSON());
+  };
+
+  const callData = (await getDoc(callDoc)).data();
+
+  const offerDescription = callData.offer;
+  await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+  const answerDescription = await pc.createAnswer();
+  await pc.setLocalDescription(answerDescription);
+
+  const answer = {
+    type: answerDescription.type,
+    sdp: answerDescription.sdp,
+  };
+
+  await updateDoc(callDoc, { answer });
+ hangCallDoc = callDoc;
+
+
+  onSnapshot(offerCandidates, (snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      console.log(change);
+      if (change.type === "added") {
+        let data = change.doc.data();
+        pc.addIceCandidate(new RTCIceCandidate(data));
+      }
+    });
+  });
+};
+
+// 4.HANGUP
+
+const hangupCall = async () => {
+  // Stop all transceivers on the connection
+  pc.getTransceivers().forEach((transceiver) => {
+    transceiver.stop();
+  });
+
+  // Stop all streams
+  if (localStream) {
+    localStream.getTracks().forEach((track) => track.stop());
+    localStream = null;
+  }
+
+  if (remoteStream) {
+    remoteStream.getTracks().forEach((track) => track.stop());
+    remoteStream = null;
+  }
+
+  // Close the peer connection
+  pc.close();
+
+  // Reset UI elements
+  callButton.disabled = true;
+  answerButton.disabled = true;
+  hangupButton.disabled = true;
+  callInput.value = "";
+  webcamButton.disabled = false;
+};
+
+
+hangupButton.onclick = async () => {
+  const callId = callInput.value;
+  const callDoc = doc(firestore, "calls", callId);
+
+  // Update the callActive status to false before deleting the document
+  await updateDoc(callDoc, { callActive: false });
+
+  //  Delay deletion to make sure the other client gets the update
+  setTimeout(async () => {
+    await deleteDoc(callDoc);
+  }, 1000);
+
+  hangupCall();
+};
+
+setInterval(() => {
+  // Listen for call hangup
+  if(hangCallDoc){
+  onSnapshot(hangCallDoc, (snapshot) => {
+    const data = snapshot.data();
+    console.log(data.callActive);
+    if (data && !data.callActive) {
+      hangupCall();
+    }
+  });}
+}, 1000);
